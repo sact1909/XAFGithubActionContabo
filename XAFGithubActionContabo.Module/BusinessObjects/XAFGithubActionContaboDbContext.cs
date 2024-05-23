19e62ad350e6155fbf8c9d0d03025a07f18f5093
Steven Checo <steven.checo.19@gmail.com>
2024-05-23 12:38:09 -0400

    first commit

---
+++ XAFGithubActionContabo.Module/BusinessObjects/XAFGithubActionContaboDbContext.cs
@@ -0,0 +1,57 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+
+namespace XAFGithubActionContabo.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class XAFGithubActionContaboContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<XAFGithubActionContaboEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new XAFGithubActionContaboEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class XAFGithubActionContaboDesignTimeDbContextFactory : IDesignTimeDbContextFactory<XAFGithubActionContaboEFCoreDbContext> {
+	public XAFGithubActionContaboEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<XAFGithubActionContaboEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=XAFGithubActionContabo");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new XAFGithubActionContaboEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(XAFGithubActionContaboContextInitializer))]
+public class XAFGithubActionContaboEFCoreDbContext : DbContext {
+	public XAFGithubActionContaboEFCoreDbContext(DbContextOptions<XAFGithubActionContaboEFCoreDbContext> options) : base(options) {
+	}
+	//public DbSet<ModuleInfo> ModulesInfo { get; set; }
+	public DbSet<ModelDifference> ModelDifferences { get; set; }
+	public DbSet<ModelDifferenceAspect> ModelDifferenceAspects { get; set; }
+	public DbSet<PermissionPolicyRole> Roles { get; set; }
+	public DbSet<XAFGithubActionContabo.Module.BusinessObjects.ApplicationUser> Users { get; set; }
+    public DbSet<XAFGithubActionContabo.Module.BusinessObjects.ApplicationUserLoginInfo> UserLoginInfos { get; set; }
+	public DbSet<DashboardData> DashboardData { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.UsePropertyAccessMode(PropertyAccessMode.PreferFieldDuringConstruction);
+        modelBuilder.Entity<XAFGithubActionContabo.Module.BusinessObjects.ApplicationUserLoginInfo>(b => {
+            b.HasIndex(nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.LoginProviderName), nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.ProviderUserKey)).IsUnique();
+        });
+        modelBuilder.Entity<ModelDifference>()
+            .HasMany(t => t.Aspects)
+            .WithOne(t => t.Owner)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
